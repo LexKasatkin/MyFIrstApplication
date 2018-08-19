d4e8d59f1289578c902bc6fb95b647b40b196551
lex <kasatkin-11@yandex.ru>
2018-08-19 19:47:15 +0700

    Task 1

--- app/src/main/java/com/example/lex/myfirstapplication/InputActivity.java
+++ app/src/main/java/com/example/lex/myfirstapplication/InputActivity.java
@@ -2,12 +2,30 @@ package com.example.lex.myfirstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InputActivity extends AppCompatActivity {
-
+    private EditText etLogin;
+    private Button btnEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        etLogin=(EditText)findViewById(R.id.et_login);
+        btnEnter=(Button)findViewById(R.id.btn_enter);
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextUtils.isEmpty(etLogin.getText())){
+                    Toast.makeText(getApplicationContext(), etLogin.getText(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
